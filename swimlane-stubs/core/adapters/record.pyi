dc67c28a3cf5797f36782eb634216546981c9c5c
Alex Way <alexwa@softcat.com>
2023-01-16 11:32:17 +0000

    Add search types Add type hinting for records.search

--- swimlane-stubs/core/adapters/record.pyi
+++ swimlane-stubs/core/adapters/record.pyi
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Any, List, Optional, Tuple, TypedDict, Unpack
 
 from swimlane.core.bulk import Replace as Replace
 from swimlane.core.cache import check_cache as check_cache
@@ -10,9 +10,18 @@ from swimlane.utils import one_of_keyword_only as one_of_keyword_only
 from swimlane.utils import random_string as random_string
 from swimlane.utils.version import requires_swimlane_version as requires_swimlane_version
 
+from ..search import FilterSearchType, SortSearchType
+
+class SearchKwargs(TypedDict, total=False):
+    keywords: List[str]
+    limit: int
+    page_size: int
+    sort: Tuple[str, SortSearchType]
+    columns: List[str]
+
 class RecordAdapter(AppResolver):
     def get(self, tracking_id: Optional[str] = None, id: Optional[str] = None) -> Record: ...
-    def search(self, *filters, **kwargs) -> List[Record]: ...
+    def search(self, *filters: Tuple[str, FilterSearchType, Any], **kwargs: Unpack[SearchKwargs]) -> List[Record]: ...
     def create(self, **fields) -> Record: ...
     def bulk_create(self, *records) -> None: ...
     def bulk_modify(self, *filters_or_records, **kwargs): ...
--- swimlane-stubs/core/search.pyi
+++ swimlane-stubs/core/search.pyi
@@ -1,22 +1,56 @@
-EQ: str
-NOT_EQ: str
-CONTAINS: str
-EXCLUDES: str
-GT: str
-LT: str
-LTE: str
-GTE: str
-AVG: str
-COUNT: str
-SUM: str
-MIN: str
-MAX: str
-GB: str
-HOUR: str
-DAY: str
-WEEK: str
-MONTH: str
-QUARTER: str
-YEAR: str
-ASC: str
-DESC: str
+from typing import Literal, Union
+
+# Filter
+EQ: Literal["equals"]
+NOT_EQ: Literal["doesNotEqual"]
+CONTAINS: Literal["contains"]
+EXCLUDES: Literal["excludes"]
+GT: Literal["greaterThan"]
+LT: Literal["lessThan"]
+LTE: Literal["lessThanOrEqual"]
+GTE: Literal["greaterThanOrEqual"]
+
+FilterSearchType = Union[
+    Literal["equals"],
+    Literal["doesNotEqual"],
+    Literal["contains"],
+    Literal["excludes"],
+    Literal["greaterThan"],
+    Literal["lessThan"],
+    Literal["lessThanOrEqual"],
+    Literal["greaterThanOrEqual"],
+]
+
+# Aggregate
+AVG: Literal["average"]
+COUNT: Literal["count"]
+SUM: Literal["sum"]
+MIN: Literal["min"]
+MAX: Literal["max"]
+
+AggregateSearchType = Union[Literal["average"], Literal["count"], Literal["sum"], Literal["min"], Literal["max"]]
+
+# GroupBy
+GB: Literal["groupBy"]
+HOUR: Literal["groupByHour"]
+DAY: Literal["groupByDay"]
+WEEK: Literal["groupByWeek"]
+MONTH: Literal["groupByMonth"]
+QUARTER: Literal["groupByQuarter"]
+YEAR: Literal["groupByYear"]
+
+GroupBySearchType = Union[
+    Literal["groupBy"],
+    Literal["groupByHour"],
+    Literal["groupByDay"],
+    Literal["groupByWeek"],
+    Literal["groupByMonth"],
+    Literal["groupByQuarter"],
+    Literal["groupByYear"],
+]
+
+# Sorts
+ASC: Literal["ascending"]
+DESC: Literal["descending"]
+
+SortSearchType = Union[Literal["ascending"], Literal["descending"]]
